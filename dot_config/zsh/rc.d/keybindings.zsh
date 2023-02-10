784eee3044f8e976613957ad296a0712bdc7ffea
Daniel Wang <hello@danielwang.dev>
2023-02-09 21:14:33 -0600

    style: match shell parameter expansion format

--- dot_config/zsh/rc.d/keybindings.zsh
+++ dot_config/zsh/rc.d/keybindings.zsh
@@ -15,8 +15,8 @@ zle -N down-line-or-beginning-search
 
 bindkey -e
 
-bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
-bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+bindkey "$terminfo[kcuu1]" up-line-or-beginning-search
+bindkey "$terminfo[kcud1]" down-line-or-beginning-search
 bindkey "^[[1;5D" backward-word
 bindkey "^[[1;5C" forward-word
-bindkey "${terminfo[kcbt]}" reverse-menu-complete
+bindkey "$terminfo[kcbt]" reverse-menu-complete
