8a58ac2db056eaa6eb41b02c24fc08135927cd37
Daniel Wang <hello@danielwang.dev>
2024-10-30 19:19:21 -0500

    Install 1Password on Linux

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -12,6 +12,19 @@ Darwin)
     type brew >/dev/null 2>&1 || install_homebrew
     brew install --cask 1password 1password-cli
     ;;
+Linux)
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+    sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg && \
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | \
+    sudo tee /etc/apt/sources.list.d/1password.list && \
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/ && \
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+    sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol && \
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22 && \
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+    sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg && \
+    sudo apt update && sudo apt install 1password 1password-cli
+    ;;
 *)
     echo "unsupported OS"
     exit 1
